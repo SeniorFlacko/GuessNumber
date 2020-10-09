17ab878fcbea54bad59cdd0c1a5bfa5df89e781e
Jesus Cristian Aleman Fuentes <jesus.aleman@globant.com>
2020-10-09 16:43:27 -0500

    adding shadow box

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -34,6 +34,15 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: '80%',
     alignItems: 'center',
+
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.26,
+
+    elevation: 8,
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
   },
   buttonContainer: {
     flexDirection: 'row',
