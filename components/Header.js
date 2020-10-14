e2dfc6b99e3de3f13c8f5dc86120eb31345cbf96
Jesus Cristian Aleman Fuentes <jesus.aleman@globant.com>
2020-10-14 16:08:14 -0500

    using platform select and merging styles

--- components/Header.js
+++ components/Header.js
@@ -4,22 +4,36 @@ import Colors from '../constants/Colors';
 
 const Header = (props) => {
   return (
-    <View style={styles.header}>
+    <View
+      style={{
+        ...styles.headerBase,
+        ...Platform.select({
+          ios: styles.headerIOS,
+          android: styles.headerAndroid,
+        }),
+      }}>
       <Text style={styles.headerTitle}>{props.title}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  header: {
+  headerBase: {
     width: '100%',
     height: 90,
     paddingTop: 36,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
-    borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
-    borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
+  },
+  headerIOS: {
+    backgroundColor: 'white',
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  },
+  headerAndroid: {
+    backgroundColor: Colors.primary,
+    borderBottomColor: 'transparent',
+    borderBottomWidth: 0,
   },
   headerTitle: {
     fontSize: 18,
